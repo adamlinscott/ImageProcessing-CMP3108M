71e25970c59c60685801fa5aea3661d361653e7c
Adam Linscott <13459186@students.lincoln.ac.uk>
2016-11-26 13:21:01 +0000

    Input files

---
+++ RuneMe.m
@@ -0,0 +1,17 @@
+% MATLAB script for Assessment Item-1
+close all;
+
+% Step-1: Load input image
+I = imread('AssignmentInput.jpg');
+figure;
+imshow(I);
+title('Step-1: Load input image');
+
+% Step-2: Conversion of input image to greyscale
+% for you to fill in ??????????
+figure;
+imshow(I);
+title('Step-2: Conversion of input image to greyscale');
+
+% Step-3: Noise removal
+
